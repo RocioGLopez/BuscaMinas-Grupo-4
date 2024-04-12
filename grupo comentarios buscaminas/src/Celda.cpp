1d64cc1ecec1ed3c8f6e60717ecb88e4db2d4fda
mishelloeiza <dloeizar@miumg.edu.gt>
2024-04-12 16:57:54 -0600

    minajugador

--- grupo comentarios buscaminas/src/Celda.cpp
+++ grupo comentarios buscaminas/src/Celda.cpp
@@ -1,81 +1,56 @@
-// Inclusión de la cabecera de la clase Celda
-#include "Celda.h"
-#include <iostream> // Inclusión de la librería de entrada/salida estándar
-using namespace std;   // Utilizar el espacio de nombres estándar
-
-// Constructor por defecto de la clase Celda
-Celda::Celda()
+#include "Celda.h" //se manda a llamar al encabezado celda.h y su contenido dentro
+#include <iostream>
+using namespace std;
+Celda::Celda() //Se inicializa el constructor llamado celda
 {
 }
-
-// Constructor de la clase Celda con parámetros
-// Recibe las coordenadas x e y y el estado de la mina
-Celda::Celda(int coordenadaX, int coordenadaY, bool estadoMina)
+Celda::Celda(int coordenadaX, int coordenadaY, bool estadoMina) //Se envian los parametros hacia el constructor hacia la funciï¿½n recibiendo coordenadas x,y y el boolean
 {
-    this->coordenadaX = coordenadaX;    // Asigna el valor de coordenadaX
-    this->coordenadaY = coordenadaY;    // Asigna el valor de coordenadaY
-    this->mina = estadoMina;            // Asigna el valor de estadoMina
-    this->minaDescubierta = false;      // Inicializa el valor de minaDescubierta en false
+    this->coordenadaX = coordenadaX;    //Puntero this para seï¿½alar que la coordenada x equivale a la coordenada x, es decir guarda el contenido en memoria de coordenada x dentro de la coordenada
+    this->coordenadaY = coordenadaY;
+    this->mina = estadoMina;            //Guarda el contenido en memoria de la variable Mina dentro de estadoMina
+    this->minaDescubierta = false;
 }
-
-// Función para establecer la coordenada X de la celda
-int Celda::setCoordenadaX(int coordenadaX)
+int Celda::setCoordenadaX(int coordenadaX)  //Usando set se establece la coordenada x llamando los parametros de la funcion CoordenadaX
 {
-    this->coordenadaX = coordenadaX;    // Asigna el valor de coordenadaX
+    this->coordenadaX = coordenadaX;
 }
-
-// Función para obtener la coordenada X de la celda
-int Celda::getCoordenadaX()
+int Celda::getCoordenadaX()         //Llamamos el valor de la funcion privada con getCoordenadaX
 {
-    return this->coordenadaX;          // Devuelve el valor de coordenadaX
+    return this->coordenadaX;       //Regresamos o returna el valor en memoria de la coordenadaX
 }
-
-// Función para establecer la coordenada Y de la celda
-int Celda::setCoordenadaY(int coordenadaY)
+int Celda::setCoordenadaY(int coordenadaY) //Usando set se establece la coordenada y llamando los parametros de la funcion Coordenaday
 {
-    this->coordenadaY = coordenadaY;    // Asigna el valor de coordenadaY
+    this->coordenadaY = coordenadaY; //Puntero que guarda en memoria los datos de la coordenada dentro del constructor
 }
-
-// Función para obtener la coordenada Y de la celda
-int Celda::getCoordenadaY()
+int Celda::getCoordenadaY() //Llamamos el valor de la funcion privada con getCoordenadaY
 {
-    return this->coordenadaY;          // Devuelve el valor de coordenadaY
+    return this->coordenadaY; //Regresamos o returna el valor en memoria de la coordenadaY
 }
-
-// Función para establecer el estado de la mina de la celda
-bool Celda::setMina(bool estadoMina)
+bool Celda::setMina(bool estadoMina)    //Se establece el valor de la mina con SetMina enviando los parametros de estadoMIna
 {
-    if (this->getMina())              // Si ya hay una mina
+    if (this->getMina())        //Si el valor de la mina es el valor de la funcion privada de Mina entonces
     {
-        return false;                 // No se puede establecer una mina
+        return false;          //regresa o returna que el valor es falso es decir, el booleando esta apagado
     }
-    else{
-        this->mina = estadoMina;      // Sino, asigna el valor de estadoMina
-        return true;                 // Y devuelve true
+    else{                           //De lo contrario
+        this->mina = estadoMina;        //el valor de la mina guardado en memoria se pasara a estadoMina y se guardara en memoria
+        return true;                //Para despues devolver que el boolean esta encendido, es verdadero.
     }
 }
-
-// Función para obtener el estado de la mina de la celda
-bool Celda::getMina()
+bool Celda::getMina()  //Se llama el valor de la funcion privada Mina, usando el get
 {
-    return this->mina;                // Devuelve el valor de mina
+    return this->mina;  //Regresa la variable usando el puntero guaradando en memoria el valor de mina
 }
-
-// Función para establecer si la mina ha sido descubierta o no
-bool Celda::setMinaDescubierta(bool minaDescubierta)
+bool Celda::setMinaDescubierta(bool minaDescubierta) //Establece el valor de la MinaDescubierta utilizando el parametro enviado de minaDescubierta
 {
-    this->minaDescubierta = minaDescubierta; // Asigna el valor de minaDescubierta
+    this->minaDescubierta = minaDescubierta; //el puntero guarda en memoria el valor de minaDescubierta en minaDescubierta
 }
-
-// Función para obtener si la mina ha sido descubierta o no
-bool Celda::getMinaDescubierta()
+bool Celda::getMinaDescubierta()    //Se llama el valor de la funcion privada MinaDescubierta
 {
-    return this->minaDescubierta;    // Devuelve el valor de minaDescubierta
+    return this->minaDescubierta;   //Regresa el valor del puntero guardado en memoria de la variable minaDescubierta
 }
-
-// Función para imprimir la celda con sus atributos
-void Celda::imprimirCelda()
+void Celda::imprimirCelda()     //Constructor que imprime la celda
 {
-    cout << "Celda en " << this->coordenadaX << ", " << this->coordenadaY << " con mina? " << this->mina << "\n";
-    // Imprime la celda con sus coordenadas y el estado de la mina
-}
+    cout << "Celda en " << this->coordenadaX << ", " << this->coordenadaY << " con mina? " << this->mina << "\n"; //Imprime el valor mostrado en pantalla
+}   //cenda en, "Coordenada en x" , "Coordenada en y" y con mina valor de la variable mina.
